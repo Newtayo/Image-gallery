ec7a9d4a08a5563e96d324035cce28be13bc0086
Newtayo <akandeabdul@gmail.com>
2023-09-22 11:51:32 +0100

    Fix Authentication error

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,37 @@
 import React, { useRef, useState, useEffect } from 'react';
-
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ImageCard from './ImageCard';
 import '../styles/home.css';
-import gif from '../assets/loading.gif'
+import gif from '../assets/loading.gif';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  const { movieList, isLoading, error } = useSelector((state) => state.movie);
+  const user = localStorage.getItem('user');
+  const { movieList, isLoading } = useSelector((state) => state.movie);
+  const navigate = useNavigate();
 
-  // Use the custom hook to manage draggedList
-  const [draggedList, setDraggedList] = useLocalStorage('draggedList', movieList);
-  const [movieSearch, setMovieSearch] = useState("")
+  useEffect(() => {
+    if (!user) {
+      navigate('/');
+    }
+  }, [user, navigate]);
 
+  const [movieSearch, setMovieSearch] = useState('');
+  const [draggedList, setDraggedList] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      const storedValue = localStorage.getItem('draggedList');
+      const initial = storedValue ? JSON.parse(storedValue) : movieList;
+      setDraggedList(initial);
+    }
+  }, [user, movieList]);
 
   const dragItem = useRef(null);
   const dragItemOver = useRef(null);
 
   const dragStarted = (e, index) => {
-    e.dataTransfer.setData("text/plain", index);
+    e.dataTransfer.setData('text/plain', index);
     dragItem.current = index;
   };
 
@@ -29,72 +43,65 @@ const Home = () => {
   };
 
   const dragEnd = () => {
-    // Rearrange the list based on drag-and-drop
     if (dragItem.current !== null && dragItemOver.current !== null) {
       const updatedList = [...draggedList];
       const [draggedItem] = updatedList.splice(dragItem.current, 1);
       updatedList.splice(dragItemOver.current, 0, draggedItem);
       setDraggedList(updatedList);
+      localStorage.setItem('draggedList', JSON.stringify(updatedList));
     }
-
     dragItem.current = null;
     dragItemOver.current = null;
   };
-  const searchMovie =(e)=>{
+
+  const searchMovie = (e) => {
     e.preventDefault();
-    const filteredMovies = movieList.filter((movie) =>
-    movie.title.toLowerCase().includes(movieSearch.toLowerCase()))
-    setDraggedList(filteredMovies)
-  }
-  if(isLoading){
-    return(
-        <div className='home-page'>
-        <img src={gif} alt='gif'/>
-        </div>
-    )
-  }
-  else{
-  return (
-    <div className='home-page'>
+    const filteredMovies = movieList.filter(
+      (movie) =>
+        movie.title.toLowerCase().includes(movieSearch.toLowerCase())
+    );
+    setDraggedList(filteredMovies);
+    localStorage.setItem('draggedList', JSON.stringify(filteredMovies));
+  };
+
+  if (isLoading) {
+    return (
+      <div className="home-page">
+        <img src={gif} alt="gif" />
+      </div>
+    );
+  } else {
+    return (
+      <div className="home-page">
         <h2> Welcome to Tayo's Movie Gallery</h2>
-        <p>You can drag a movie and drop in at any point in the page</p>
+        <p>You can drag a movie and drop it anywhere on the page</p>
         <form onSubmit={searchMovie}>
-            <input type='text' placeholder='Search a movie'
-             value={movieSearch} 
-             onChange={(e)=>setMovieSearch(e.target.value)}
-             />
-            <button type='submit'>Search</button>
+          <input
+            type="text"
+            placeholder="Search a movie"
+            value={movieSearch}
+            onChange={(e) => setMovieSearch(e.target.value)}
+          />
+          <button type="submit">Search</button>
         </form>
 
-      <div className="movie-list">
-        {draggedList.map((item, index) => (
-          <div
-            key={index}
-            draggable
-            onDragStart={(e) => dragStarted(e, index)}
-            onDragOver={(e) => dragOver(e, index)}
-            onDragEnd={() => dragEnd()}
-            className="card"
-          >
-            <ImageCard image={item} />
-          </div>
-        ))}
+        <div className="movie-list">
+          {draggedList.map((item, index) => (
+            <div
+              key={index}
+              draggable
+              onDragStart={(e) => dragStarted(e, index)}
+              onDragOver={(e) => dragOver(e, index)}
+              onDragEnd={() => dragEnd()}
+              className="card"
+            >
+              <ImageCard image={item} />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
-}};
-
-// Custom hook for syncing state with local storage
-function useLocalStorage(key, initialValue) {
-  const storedValue = localStorage.getItem(key);
-  const initial = storedValue ? JSON.parse(storedValue) : initialValue;
-  const [value, setValue] = useState(initial);
-
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [key, value]);
-
-  return [value, setValue];
-}
+    );
+  }
+};
 
 export default Home;
